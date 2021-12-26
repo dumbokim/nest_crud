4a01417e42070dcb13c93e23fe3ddb2a724baf1f
DumboKim <rlawlsgus97@gmail.com>
2021-12-26 20:34:54 +0900

    Dto, 삭제 까지 추가

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Board } from './boards.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  // boardsService: BoardsService;
+
+  // constructor(boardsService: BoardsService) {
+  //   this.boardsService = boardsService;
+  // }
+
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+}
