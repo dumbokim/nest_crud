fce58cf9d5fb1898d475640f628563f0dc178298
DumboKim <rlawlsgus97@gmail.com>
2021-12-30 03:47:15 +0900

    postgres 설치, 연동, repository pattern 진행중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/configs/typeorm.config';
 import { TableModule } from './table/table.module';
 
 @Module({
-  imports: [BoardsModule, TableModule],
+  imports: [BoardsModule, TableModule, TypeOrmModule.forRoot(typeORMConfig)],
 })
 export class AppModule {}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,46 +1,59 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { v1 as uuid } from 'uuid';
-import { Board, BoardStatus } from './boards.model';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
+import { BoardStatus } from './board.status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
 
-  getAllBoards(): Board[] {
-    return this.boards;
+  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto);
   }
 
-  createBoard(createBoardDto: CreateBoardDto): Board {
-    const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.boards.push(board);
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
 
     if (!found) {
-      throw new NotFoundException(`Can't find board id : ${id}`);
+      throw new NotFoundException(`Can't find id : ${id}`);
     }
 
     return found;
   }
-
-  deleteBoard(id: string): void {
-    this.boards = this.boards.filter((board) => board.id !== id);
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
+  // private boards: Board[] = [];
+  // getAllBoards(): Board[] {
+  //   return this.boards;
+  // }
+  // createBoard(createBoardDto: CreateBoardDto): Board {
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  // getBoardById(id: string): Board {
+  //   const found = this.boards.find((board) => board.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Can't find board id : ${id}`);
+  //   }
+  //   return found;
+  // }
+  // deleteBoard(id: string): void {
+  //   this.boards = this.boards.filter((board) => board.id !== id);
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board = this.getBoardById(id);
+  //   board.status = status;
+  //   return board;
+  // }
 }
