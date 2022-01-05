5d2862767aa5c2f18cac7c523d5740f3dbf43183
DumboKim <rlawlsgus97@gmail.com>
2022-01-05 23:58:24 +0900

    bcrypt 암호화 비밀번호 저장

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,21 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
+
+    const salt = await bcrypt.genSalt();
+    console.log('salt', salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log('hashed', hashedPassword);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
