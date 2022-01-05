ab15c49856b44ab689496e54340481d35e8d7061
DumboKim <rlawlsgus97@gmail.com>
2022-01-05 23:47:18 +0900

    가입 기능 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
-import { typeORMConfig } from './boards/configs/typeorm.config';
+import { typeORMConfig } from './configs/typeorm.config';
 import { TableModule } from './table/table.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardsModule, TableModule, TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [
+    BoardsModule,
+    TableModule,
+    TypeOrmModule.forRoot(typeORMConfig),
+    AuthModule,
+  ],
 })
 export class AppModule {}
