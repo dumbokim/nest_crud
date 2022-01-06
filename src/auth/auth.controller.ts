1852ad68593b3e399a88dd36120509ac55558512
DumboKim <rlawlsgus97@gmail.com>
2022-01-07 01:28:41 +0900

    jwt accesstoken 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
   @Post('/signin')
   signIn(
     @Body(ValidationPipe) AuthCredentialDto: AuthCredentialDto,
-  ): Promise<string> {
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(AuthCredentialDto);
   }
 }
